a08f92be2b381696ab1a09c7def92f19e51f68b3
helineto10 <heli.neto10@gmail.com>
2022-06-08 19:40:26 -0300

    Página Home ok, Página newproject em andamento

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './Home.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
 
 function Home() {
   return (
-    <div>
-        <h1>Home</h1>
-    </div>
+    <section className='homeContainer'>
+        <h1>Bem-vindo ao <span>Costs</span></h1>
+        <p>Comece a gerenciar seus projetos agora mesmo!</p>
+        <LinkButton to='/newproject' text='Criar Projeto' />
+        <img src={savings} alt='Costs'/>
+    </section>
   )
 }
 
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import ProjectForm from '../project/ProjectForm'
+import './NewProject.css'
 
 function NewProject() {
   return (
-    <div>
-        <h1>New Project</h1>
+    <div className="newProjectContainer">
+        <h1>Criar Projeto</h1>
+        <p>Crie seu projeto para depois adicionar os serviços</p>
+        <ProjectForm />
     </div>
   )
 }
