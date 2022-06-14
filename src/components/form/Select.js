158ee543ca7b5ee61b9278095fd2f475258d78b9
Heli Rufino <heli.neto10@gmail.com>
2022-06-13 21:45:12 -0300

    conexao com api criada via fetch ok, categorias no select ok

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -5,13 +5,13 @@ function Select({ text, name, options, handleOnChange, value }) {
   return (
     <div className="form_control">
       <label htmlFor={name}>{text}</label>
-      <select
-        id={name}
-        name={name}
-        onChange={handleOnChange}
-        value={value}
-      >
-          <option>Selecione uma opção</option>
+      <select id={name} name={name} onChange={handleOnChange} value={value}>
+        <option>Selecione uma opção</option>
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
       </select>
     </div>
   );
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./ProjectForm.css";
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
 function ProjectForm({btnText}) {
+
+const [categories, setCategories] = useState([])
+
+const url = "http://localhost:5000"
+
+useEffect(() => {
+  fetch(`${url}/categories`, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then((resp) => resp.json())
+  .then((data) => setCategories(data))
+  .catch((e) => console.log(e))
+}, [])
+
+
+
   return (
     <form className="form">
       <Input
@@ -19,7 +38,8 @@ function ProjectForm({btnText}) {
         placeholder="Insira o orçamento total"
         name="budget"
       />
-      <Select name='category_id' text='Selecione a categoria'  />
+      <Select name='category_id' text='Selecione a categoria'
+      options={categories}  />
       <SubmitButton text={btnText} />
     </form>
   );
